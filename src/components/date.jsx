77faf420c7071da237ad119a27b5d9080336059e
manjeetdhayal <manjeetdhayal786@gmail.com>
2021-07-27 10:43:58 +0530

    social profiles added

---
+++ src/components/date.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+// date component of the page
+function Date() {
+    return (
+        <div className = "date-component" >
+            Sept. 18-19, 2021
+        </div>
+    )
+}
+
+export default Date; 
